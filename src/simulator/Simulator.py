5a66700bd1557eaf4f767d3e0fe6145f9aef0466
davidedomini <davide.domini@unibo.it>
2024-12-30 11:46:03 +0100

    feat: implement client-server interaction

--- src/simulator/Simulator.py
+++ src/simulator/Simulator.py
@@ -1,5 +1,6 @@
+from client.FedAvgClient import FedAvgClient
+from client.ScaffoldClient import ScaffoldClient
 from server.ScaffoldServer import ScaffoldServer
-
 
 class Simulator:
 
@@ -9,18 +10,27 @@ class Simulator:
         self.areas = areas
         self.dataset = dataset
         self.n_clients = n_clients
-        self.clients = []
+        self.clients = self.initialize_clients()
         self.server = self.initialize_server()
-
 
     def seed_everything(self, seed):
         pass
 
+    def start(self, global_rounds):
+        for round in range(global_rounds):
+            print(f'Starting global round {round}')
+            self.notify_clients()
+            self.clients_update()
+            self.notify_server()
+            self.server_update()
 
-    def start(self):
-        for name, param in self.server.control_state.named_parameters():
-            print(f'{name} --> {param}')
-
+    def initialize_clients(self):
+        if self.algorithm == 'fedavg':
+            return [FedAvgClient(self.dataset, 32, 2) for _ in range(self.n_clients)]
+        elif self.algorithm == 'scaffold':
+            return [ScaffoldClient(self.dataset, 32, 2) for _ in range(self.n_clients)]
+        else:
+            raise Exception(f'Algorithm {self.algorithm} not supported! Please check :)')
 
     def initialize_server(self):
         if self.algorithm == 'scaffold':
@@ -28,6 +38,28 @@ class Simulator:
         else:
             raise Exception(f'Algorithm {self.algorithm} not supported! Please check :)')
 
+    def notify_clients(self):
+        for client in self.clients:
+            if self.algorithm == 'fedavg':
+                client.notify_updates(self.server.model)
+            elif self.algorithm == 'scaffold':
+                client.notify_updates(self.server.model, self.server.control_state)
+
+    def clients_update(self):
+        for client in self.clients:
+            client.train()
+
+    def notify_server(self):
+        client_data = {}
+        for index, client in enumerate(self.clients):
+            if self.algorithm == 'fedavg':
+                client_data[index] = client.model
+            elif self.algorithm == 'scaffold':
+                client_data[index] = { 'model': client.model, 'client_control_state': client.client_control_state }
+        self.server.receive_client_update(client_data)
+
+    def server_update(self):
+        self.server.aggregate()
 
     def export_data(self):
         """
