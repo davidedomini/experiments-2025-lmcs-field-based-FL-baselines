07b6a9ca5e6dd74953524d0da012632a2d4219c1
davidedomini <davide.domini@unibo.it>
2024-12-30 17:36:31 +0100

    feat: implement function to compute hard partitioning mapping

--- src/utils/FedUtils.py
+++ src/utils/FedUtils.py
@@ -1,6 +1,7 @@
-from models.MNIST import NNMnist
-import torch.nn as nn
 import torch
+import numpy as np
+import torch.nn as nn
+from models.MNIST import NNMnist
 
 def initialize_model(name):
     if name == 'MNIST':
@@ -8,9 +9,17 @@ def initialize_model(name):
     else:
         raise Exception(f'Model {name} not implemented! Please check :)')
 
-
 def initialize_control_state(model, experiment):
     control_state = initialize_model(experiment)
     for param in control_state.parameters():
         nn.init.constant_(param, 0.0)
     return control_state
+
+def hard_non_iid_mapping(areas: int, labels: int) -> np.ndarray:
+    labels_set = np.arange(labels)
+    split_classes_per_area = np.array_split(labels_set, areas)
+    distribution = np.zeros((areas, labels))
+    for i, elems in enumerate(split_classes_per_area):
+        rows = [i for _ in elems]
+        distribution[rows, elems] = 1
+    return distribution
