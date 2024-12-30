9ee4f946657d936abb185c2941c2f0b0a9ae888c
davidedomini <davide.domini@unibo.it>
2024-12-30 17:37:51 +0100

    feat: implement function to compute partitioning given a data distribution

--- src/utils/FedUtils.py
+++ src/utils/FedUtils.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import torch.nn as nn
 from models.MNIST import NNMnist
+from torch.utils.data import Dataset, Subset
 
 def initialize_model(name):
     if name == 'MNIST':
@@ -23,3 +24,22 @@ def hard_non_iid_mapping(areas: int, labels: int) -> np.ndarray:
         rows = [i for _ in elems]
         distribution[rows, elems] = 1
     return distribution
+
+def partitioning(distribution: np.ndarray, dataset: Dataset) -> dict[int, list[int]]:
+    targets = dataset.targets
+    areas = distribution.shape[0]
+    targets_cardinality = distribution.shape[1]
+    class_counts = torch.bincount(targets)
+    partitions = {}
+    for area in range(areas):
+        area_distribution = distribution[area, :]
+        elements_per_class = torch.tensor(area_distribution) * class_counts
+        elements_per_class = torch.floor(elements_per_class).to(torch.int)
+        selected_indices = []
+        for label in range(targets_cardinality):
+            target_indices = torch.where(targets == label)[0]
+            selected_count = min(len(target_indices), elements_per_class[label].item())
+            if selected_count > 0:
+                selected_indices.extend(target_indices[:selected_count].tolist())
+        partitions[area] = selected_indices
+    return partitions
