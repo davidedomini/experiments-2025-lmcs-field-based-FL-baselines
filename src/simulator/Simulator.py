fix: using global model for validation at each global round instead of average of local models performance

src/simulator/Simulator.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import pandas as pd
 import utils.FedUtils as utils
-from torch.utils.data import Dataset, Subset
+from torch.utils.data import Dataset, Subset, random_split
 from torchvision import datasets, transforms
 from client.FedAvgClient import FedAvgClient
 from server.FedAvgServer import FedAvgServer
@@ -13,17 +13,18 @@ from server.ScaffoldServer import ScaffoldServer
 class Simulator:
 
     def __init__(self, algorithm, partitioning, areas, dataset_name, n_clients, data_folder):
+        self.batch_size = 32
+        self.local_epochs = 2
+        self.dataset_name = dataset_name
+        self.complete_dataset, self.training_data, self.validation_data = self.initialize_data()
         self.partitioning = partitioning
         self.algorithm = algorithm
         self.areas = areas
-        self.dataset_name = dataset_name
         self.n_clients = n_clients
         self.export_path = f'{data_folder}/algorithm-{self.algorithm}_dataset-{dataset_name}_partitioning-{self.partitioning}_areas-{self.areas}_clients-{self.n_clients}'
         self.simulation_data = pd.DataFrame(columns=['Round','TrainingLoss', 'ValidationLoss', 'ValidationAccuracy'])
         self.clients = self.initialize_clients()
         self.server = self.initialize_server()
-        self.batch_size = 32
-        self.local_epochs = 2
 
     def seed_everything(self, seed):
         random.seed(seed)
@@ -36,10 +37,12 @@ class Simulator:
         for r in range(global_rounds):
             print(f'Starting global round {r}')
             self.notify_clients()
-            self.clients_update(r)
+            training_loss = self.clients_update()
             self.notify_server()
             self.server_update()
-        self.test_global_model()
+            validation_loss, validation_accuracy = self.test_global_model()
+            self.export_data(r, training_loss, validation_loss, validation_accuracy)
+        self.test_global_model(False)
         self.save_data()
 
     def initialize_clients(self):
@@ -66,20 +69,21 @@ class Simulator:
             elif self.algorithm == 'scaffold':
                 client.notify_updates(self.server.model, self.server.control_state)
 
-    def clients_update(self, global_round):
+    def clients_update(self):
         training_losses = []
-        evaluation_losses = []
-        evaluation_accuracies = []
+        # evaluation_losses = []
+        # evaluation_accuracies = []
         for client in self.clients:
             train_loss = client.train()
-            evaluation_loss, evaluation_accuracy = client.evaluate()
+            # evaluation_loss, evaluation_accuracy = client.evaluate()
             training_losses.append(train_loss)
-            evaluation_losses.append(evaluation_loss)
-            evaluation_accuracies.append(evaluation_accuracy)
+            # evaluation_losses.append(evaluation_loss)
+            # evaluation_accuracies.append(evaluation_accuracy)
         average_training_loss = sum(training_losses) / len(training_losses)
-        average_evaluation_loss = sum(evaluation_losses) / len(evaluation_losses)
-        average_evaluation_accuracy = sum(evaluation_accuracies) / len(evaluation_accuracies)
-        self.export_data(global_round, average_training_loss, average_evaluation_loss, average_evaluation_accuracy)
+        # average_evaluation_loss = sum(evaluation_losses) / len(evaluation_losses)
+        # average_evaluation_accuracy = sum(evaluation_accuracies) / len(evaluation_accuracies)
+        # self.export_data(global_round, average_training_loss, average_evaluation_loss, average_evaluation_accuracy)
+        return average_training_loss
 
     def notify_server(self):
         client_data = {}
@@ -93,17 +97,27 @@ class Simulator:
     def server_update(self):
         self.server.aggregate()
 
-    def map_client_to_data(self) -> dict[int, Subset]:
+    def initialize_data(self):
         d = self.get_dataset()
+        dataset_size = len(d)
+        training_size = int(dataset_size * 0.8)
+        validation_size = dataset_size - training_size
+        training_data, validation_data = random_split(d, [training_size, validation_size])
+        return d, training_data, validation_data
+
+    def map_client_to_data(self) -> dict[int, Subset]:
         clients_split = np.array_split(list(range(self.n_clients)), self.areas)
         mapping_area_clients = { areaId: list(clients_split[areaId]) for areaId in range(self.areas) }
         if self.partitioning.lower() == 'hard':
-            mapping = utils.hard_non_iid_mapping(self.areas, len(d.classes))
+            mapping = utils.hard_non_iid_mapping(self.areas, len(self.complete_dataset.classes))
         elif self.partitioning.lower() == 'iid':
-            mapping = utils.iid_mapping(self.areas, len(d.classes))
+            mapping = utils.iid_mapping(self.areas, len(self.complete_dataset.classes))
         else:
             raise Exception(f'Partitioning {self.partitioning} not supported! Please check :)')
-        distribution_per_area = utils.partitioning(mapping, d)
+        print(mapping)
+        print(self.complete_dataset.targets.shape)
+        print(self.complete_dataset.targets[self.training_data.indices].shape)
+        distribution_per_area = utils.partitioning(mapping, self.complete_dataset.targets[self.training_data.indices])
         mapping_client_data = {}
         for area in mapping_area_clients.keys():
             clients = mapping_area_clients[area]
@@ -111,14 +125,18 @@ class Simulator:
             random.shuffle(indexes)
             split = np.array_split(indexes, len(clients))
             for i, c in enumerate(clients):
-                mapping_client_data[c] = Subset(d, split[i])
+                mapping_client_data[c] = Subset(self.training_data, split[i])
         return mapping_client_data
 
-    def test_global_model(self):
+    def test_global_model(self, validation = True):
         model = self.server.model
-        dataset = self.get_dataset(False)
-        _, accuracy = utils.test_model(model, dataset)
-        print(f'Test accuracy {accuracy}')
+        if validation:
+            dataset = self.validation_data
+        else:
+            dataset = self.get_dataset(False)
+        loss, accuracy = utils.test_model(model, dataset, self.batch_size)
+        print(f'Test accuracy {accuracy} test loss {loss}')
+        return loss, accuracy
 
     def get_dataset(self, train = True):
         transform = transforms.Compose([transforms.ToTensor()])