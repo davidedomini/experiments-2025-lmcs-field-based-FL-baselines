2c1b0ace384a32c93c920b8ed884a374331ee287
davidedomini <davide.domini@unibo.it>
2025-01-02 17:04:16 +0100

    chore: remove useless commented code

--- src/client/FedAvgClient.py
+++ src/client/FedAvgClient.py
@@ -12,10 +12,6 @@ class FedAvgClient:
         self.batch_size = batch_size
         self.epochs = epochs
         self._model = initialize_model(dataset_name)
-        # dataset_size = len(self.dataset)
-        # training_size = int(dataset_size * 0.8)
-        # validation_size = dataset_size - training_size
-        # self.training_set, self.validation_set = random_split(self.dataset, [training_size, validation_size])
         self.training_set = dataset
         self.lr = 0.001
         self.weight_decay=1e-4
@@ -41,22 +37,6 @@ class FedAvgClient:
             mean_epoch_loss = sum(batch_losses) / len(batch_losses)
             losses.append(mean_epoch_loss)
         return sum(losses) / len(losses)
-
-    # def evaluate(self):
-    #     criterion = nn.NLLLoss()
-    #     self._model.eval()
-    #     loss, total, correct = 0.0, 0.0, 0.0
-    #     data_loader = DataLoader(self.validation_set, batch_size=self.batch_size, shuffle=False)
-    #     for batch_index, (images, labels) in enumerate(data_loader):
-    #         outputs = self._model(images)
-    #         batch_loss = criterion(outputs, labels)
-    #         loss += batch_loss.item()
-    #         _, pred_labels = torch.max(outputs, 1)
-    #         pred_labels = pred_labels.view(-1)
-    #         correct += torch.sum(torch.eq(pred_labels, labels)).item()
-    #         total += len(labels)
-    #     accuracy = correct / total
-    #     return loss, accuracy
 
     def notify_updates(self, global_model):
         self._model.load_state_dict(global_model.state_dict())
