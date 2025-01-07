712192f7f80c7ca709aa0c132222681f3cf52fd2
Davide Domini <davide.domini@unibo.it>
2025-01-07 11:48:12 +0100

    fix: fix output size

--- src/models/MNIST.py
+++ src/models/MNIST.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 
 class NNMnist(nn.Module):
 
-    def __init__(self, h1=128):
+    def __init__(self, h1=128, output_size=10):
         super().__init__()
         self.fc1 = torch.nn.Linear(28*28, h1)
-        self.fc2 = torch.nn.Linear(h1, 27)
+        self.fc2 = torch.nn.Linear(h1, output_size)
 
     def forward(self, x):
         x = x.view(-1, 28 * 28)
